6f2843349a9f55ea2f221426ee3c67e44a37018a
Agiff <ghifarimohammadrahadian@gmail.com>
2024-07-12 19:03:09 +0700

    first commit

---
+++ 1. Basic & Variable/index.js
@@ -0,0 +1,22 @@
+console.log('Hello World');
+
+var nama = 'Agif';
+console.log(nama);
+
+nama = 'Seirei';
+console.log(nama);
+
+let job = 'Engineer';
+console.log(job);
+
+job = 'Desinger';
+console.log(job);
+
+const buah = 'Apel';
+console.log(buah);
+
+// buah = 'Jeruk';
+// console.log(buah);
+
+
+
