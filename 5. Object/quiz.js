e0fd3949de0fc20678736ab9b6335a210c038677
Agiff <ghifarimohammadrahadian@gmail.com>
2024-07-30 20:10:48 +0700

    feat: object

---
+++ 5. Object/quiz.js
@@ -0,0 +1,46 @@
+/*
+
+Quiz 1: Create a new object named "person" then fill the properties with:
+- firstName
+- lastName
+- address
+  - country
+  - city
+- age
+
+And then print the whole object
+
+*/
+
+
+
+
+/*
+
+Quiz 2: Reassign the country and city into another value, then print the object
+
+*/
+
+
+
+/*
+
+Quiz 3: Create a new object from empty object "{}", then start adding some properties
+
+*/
+
+
+
+/*
+
+Quiz 4: Print each value by using forIn
+
+*/
+
+
+
+/*
+
+Quiz 5: Print the whole value in one string
+
+*/
