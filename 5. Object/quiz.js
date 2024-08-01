16e47dff114a431b374aba3c9d7c018e98a0f435
Agiff <ghifarimohammadrahadian@gmail.com>
2024-08-01 18:59:15 +0700

    feat: array of objects

--- 5. Object/quiz.js
+++ 5. Object/quiz.js
@@ -3,7 +3,7 @@
 Quiz 1: Create a new object named "person" then fill the properties with:
 - firstName
 - lastName
-- address
+- address (object)
   - country
   - city
 - age
@@ -12,8 +12,17 @@ And then print the whole object
 
 */
 
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  address: {
+    country: 'Indonesia',
+    city: 'Jakarta'
+  },
+  age: 30
+}
 
-
+console.log(person);
 
 /*
 
@@ -21,7 +30,14 @@ Quiz 2: Reassign the country and city into another value, then print the object
 
 */
 
+person.address.country = 'Japan';
+person.address.city = 'Tokyo';
 
+// person.address = {
+//   province: 'West Java'
+// }
+
+console.log(person);
 
 /*
 
@@ -29,7 +45,13 @@ Quiz 3: Create a new object from empty object "{}", then start adding some prope
 
 */
 
+const pet = {};
 
+pet.name = 'Kaya';
+pet.type = 'Cat';
+pet.color = 'White';
+
+console.log(pet);
 
 /*
 
@@ -37,10 +59,17 @@ Quiz 4: Print each value by using forIn
 
 */
 
-
+for (const key in pet) {
+  if (Object.hasOwnProperty.call(pet, key)) {
+    const value = pet[key];
+    console.log(value);
+  }
+}
 
 /*
 
 Quiz 5: Print the whole value in one string
 
 */
+
+console.log(`I have a ${pet.type.toLowerCase()} named ${pet.name}, her color is ${pet.color.toLowerCase()}`);
---
+++ 5. Object/quizArrayOfObjects.js
@@ -0,0 +1,66 @@
+const userDB = [
+  {
+    id: 1,
+    username: 'johndoe',
+    email: 'john_doe@mail.com',
+    password: 'jdc123',
+    profile: {
+      fullName: 'John Doe',
+      age: 30,
+      address: 'Jakarta',
+      country: 'Indonesia'
+    }
+  },
+  {
+    id: 2,
+    username: 'inibudi',
+    email: 'budi@mail.com',
+    password: 'budiman',
+    profile: {
+      fullName: 'Budi Pekerti',
+      age: 20,
+      address: 'Surabaya',
+      country: 'Indonesia'
+    }
+  },
+  {
+    id: 3,
+    username: 'saskeh',
+    email: 'sasuke@akatsuki.com',
+    password: 'narto',
+    profile: {
+      fullName: 'Sasule',
+      age: 23,
+      address: 'Kyoto',
+      country: 'Japan'
+    }
+  },
+]
+
+/*
+  Quiz 1: Print last user's fullName, country, address, and age
+*/
+
+
+
+/*
+  Quiz 2: Loop the userDB and print: email, fullName, and address 
+*/
+
+
+
+/*
+  Quiz 3: Add a new user with a complete data (just hardcode the ID).
+*/
+
+
+
+/*
+  Quiz 4: Find a user with this email "budi@mail.com" and then print the object
+*/
+
+
+
+/*
+  Quiz 5 (Bonus): Find a user that lives in "Jakarta" by using .find()
+*/
