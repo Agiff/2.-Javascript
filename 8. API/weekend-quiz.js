b49f9e2f6260ba00fd99176ea8bdce7d370aef6b
Agiff <ghifarimohammadrahadian@gmail.com>
2024-08-17 13:10:09 +0700

    feat: weekend quiz promise and fetch

---
+++ 8. API/weekend-quiz.js
@@ -0,0 +1,57 @@
+/*
+  Quiz 1
+
+  Requirements:
+    - Create a function named `simplePromise` that returns a new promise.
+    - Inside the promise, use `setTimeout` to simulate an asynchronous operation (like a delay of 2 seconds).
+    - Resolve the promise with the message: "Promise resolved!" after the delay.
+
+  Bonus:
+    - Add an error condition using `Math.random()` to reject the promise with the message: "Promise rejected!" if the number is less than 0.5.
+    - Use `.then()` and `.catch()` to handle the resolved and rejected cases.
+*/
+
+
+
+/*
+  Quiz 2
+
+  Requirements:
+    - Create a function named `paramPromise` that returns a new promise.
+    - The function will receive a parameter called `message`.
+    - Inside the promise, use `setTimeout` to delay for 1 second, then resolve the promise with the `message`.
+
+  Bonus:
+    - If the `message` parameter is empty or not provided, reject the promise with the message: "No message provided!".
+    - Implement `.finally()` to display a message "Operation complete" whether the promise is resolved or rejected.
+*/
+
+
+
+/*
+  Quiz 3
+
+  Requirements:
+    - Fetch any API you can use then print the data using `.then()`.
+    - Handle the error by using `throw` and use `catch` to receive the error message.
+
+  Bonus:
+    - Manipulate the data you get from the API before printing it.
+    - Using other API than the pokemon we used is much better.
+*/
+
+
+
+/*
+  Quiz 4
+
+  Requirements:
+    - Fetch any API you can use then print the data using `Async` / `Await`.
+    - Handle the error by using `throw` and use `catch` to receive the error message.
+
+  Bonus:
+    - Manipulate the data you get from the API before printing it.
+    - Using other API than the pokemon we used is much better.
+*/
+
+
